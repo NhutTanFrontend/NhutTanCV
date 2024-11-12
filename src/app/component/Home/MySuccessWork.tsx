d0367714b86a1865474b057055723aef39076c43
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-11-12 16:41:54 +0700

    edit text color

--- src/app/component/Home/MySuccessWork.tsx
+++ src/app/component/Home/MySuccessWork.tsx
@@ -3,7 +3,9 @@
 function MySuccessWork() {
     return (
         <div className="container mx-auto">
-            <div className="w-90% mx-auto py-20"></div>
+            <div className="w-90% mx-auto py-20">
+                <h2>My Success Work</h2>
+            </div>
         </div>
     );
 }
