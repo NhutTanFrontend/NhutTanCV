96a0811d4074c17ee3dbdec20f9a35fb17def143
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-11-13 15:07:01 +0700

    edit

--- src/app/component/Home/MysuccessWorkItem.tsx
+++ src/app/component/Home/MysuccessWorkItem.tsx
@@ -22,7 +22,7 @@ function MysuccessWorkItem(props: MysuccessWorkItemProp) {
     return (
         <div className="relative w-full h-full overflow-hidden" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
             <Image src={data.image} alt="Trần Lê Nhứt Tân" loading="lazy" width={1000} height={1000} className="w-full h-full rounded-lg" />
-            <Link href={data.linkWeb} className={`absolute w-full h-full left-0 ${isHover ? 'top-0' : 'top-60'} transition-all duration-300 bg-black rounded-lg bg-opacity-55 flex flex-col items-center justify-center text-white p-5 gap-3`}>
+            <Link target='_blank' href={data.linkWeb} className={`absolute w-full h-full left-0 ${isHover ? 'top-0' : 'top-60'} transition-all duration-300 bg-black rounded-lg bg-opacity-55 flex flex-col items-center justify-center text-white p-5 gap-3`}>
                 <h3 className="roboto-medium text-2xl">{data.title}</h3>
                 <p className="text-center text-[15px]">{data.detail}</p>
             </Link>
